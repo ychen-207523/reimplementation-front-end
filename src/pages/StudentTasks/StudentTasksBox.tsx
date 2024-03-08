c761d3f948e20588c8922b4d2c5b2c3c1d743d35
Yunfei <ychen207523@gmail.com>
2024-03-07 22:31:26 -0500

    add StudentTasksBox.css

--- src/pages/StudentTasks/StudentTasksBox.tsx
+++ src/pages/StudentTasks/StudentTasksBox.tsx
@@ -1,4 +1,5 @@
-import React, { useState, useEffect, useCallback, useMemo } from 'react';
+import React from 'react';
+import "./StudentTasksBox.css"
 
 /**
  * @author Yunfei Chen on March, 2023
@@ -10,7 +11,23 @@ export const StudentTasksBox = () => {
 
   return (
     <div className="taskbox" style={{ width: '18%', display: 'inline', float: 'left', marginRight: '10px' }}>
-        <strong>Hello World</strong>
+      <strong>
+        &nbsp;&nbsp;<span className="tasknum">&nbsp;{taskNotStarted.length}&nbsp;</span> Tasks Not Started<br />
+      </strong>
+      <br />
+
+      <br />
+      <strong>
+        &nbsp;&nbsp;<span className="revnum">&nbsp;{taskRevisions.length}&nbsp;</span> Revisions<br />
+      </strong>
+      <br />
+
+      <br />
+      <strong>
+        Students who have teamed with you<br />
+      </strong>
+      <br />
+      
     </div>
   )
 
